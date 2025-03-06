01ecea83f972e739c754381e4339a483a9f0775e
Emil Johansson <Emilaik@gmail.com>
2025-03-06 18:01:09 +0100

    Refactor RandomBooksComponent to use fetchRandomBooks API and add loading state

--- src/components/randomBooks/randomBooks.tsx
+++ src/components/randomBooks/randomBooks.tsx
@@ -1,70 +1,48 @@
 import React, { useState, useEffect } from "react";
-
-export const fetchRandomBooks = async () => {
-  const url = `https://openlibrary.org/subjects/fantasy.json?limit=100`;
-  const response = await fetch(url);
-  const data = await response.json();
-
-  console.log("Total books from API:", data.works.length);
-
-  if (!data.works.length) {
-    console.log("Inga böcker hittades");
-    return [];
-  }
-
-  // Skapa en lista med böcker som har omslagsbilder
-  const booksWithCovers = data.works
-    .map((book: any) => ({
-      title: book.title,
-      coverUrl: book.cover_id
-        ? `https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`
-        : null,
-    }))
-    .filter((book) => book.coverUrl);
-
-  console.log("Books with covers:", booksWithCovers.length);
-
-  const shuffledBooks = [...booksWithCovers].sort(() => Math.random() - 0.5);
-
-  const slicedBooks = shuffledBooks.slice(0, 12);
-
-  console.log("Final books count:", slicedBooks.length);
-  return slicedBooks;
-};
+import { fetchRandomBooks } from "../../API/randomBooksApi";
 
 const RandomBooksComponent: React.FC = () => {
   const [books, setBooks] = useState<
     { title: string; coverUrl: string | null }[]
   >([]);
+  // state loading
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchBooks = async () => {
+      setLoading(true);
       const books = await fetchRandomBooks();
       setBooks(books);
+      setLoading(false);
       console.log("Number of books:", books.length);
     };
 
     fetchBooks();
   }, []);
+
   return (
     <div className="p-4 flex justify-center">
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 items-center space-x-30 justify-center pl-32">
-        {books.map((book, index) => (
-          <li
-            key={index}
-            className="flex flex-col items-center w-40 bg-white p-4 rounded-lg shadow-[0px_0px_4px_3px_rgba(195,186,171,0.3)]"
-          >
-            {book.coverUrl && (
-              <img
-                src={book.coverUrl}
-                alt={book.title}
-                className="w-50 h-40 object-contain mb-2"
-              />
-            )}
-            <h2 className="text-center break-words">{book.title}</h2>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 items-center space-x-30 justify-center pl-32">
+          {books.map((book, index) => (
+            <li
+              key={index}
+              className="flex flex-col items-center w-40 bg-white p-4 rounded-lg shadow-[0px_0px_4px_3px_rgba(195,186,171,0.3)]"
+            >
+              {book.coverUrl && (
+                <img
+                  src={book.coverUrl}
+                  alt={book.title}
+                  className="w-50 h-40 object-contain mb-2"
+                />
+              )}
+              <h2 className="text-center break-words">{book.title}</h2>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
