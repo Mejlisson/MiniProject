5214612c0fbc32109f8f9cda1285aa1ace675c84
Emil Johansson <Emilaik@gmail.com>
2025-03-21 16:28:50 +0100

    Refactor layout structure by removing old Layout component and adding Footer component

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import HamburgerMenu from "../meny/menu";
+import Footer from "../components/footer/footer";
 
 export default function Layout() {
   return (
     <>
       <HamburgerMenu />
-      <main className="p-4">
+      <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
