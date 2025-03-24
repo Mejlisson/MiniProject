6d191a8de1b71aa54b12a50381cd890ed8bc4afd
Sabina Mejlisson <sabinamejlisson@gmail.com>
2025-03-24 14:26:16 +0100

    Update footer img

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="relative bg-gradient-to-r from-gray-800 to-teal-700 text-white py-4">
+    <footer className="relative bg-gradient-to-r from-gray-800 to-teal-700 text-white py-2">
       <div className="container mx-auto flex flex-wrap justify-between items-center px-6">
         {/* Bokikon */}
         <div className="flex space-x-3">
-          <img src="/book-icon.png" alt="Book logo" className="h-10 w-10" />
+          <img src="/footer.jpg" alt="Book logo" className="h-25 w-100" />
         </div>
 
         {/* About Us-sektion */}
