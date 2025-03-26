5f58a7f9db0def5dbccae6422102e1dbd0327712
Emil Johansson <Emilaik@gmail.com>
2025-03-26 15:01:29 +0100

    Refactor BookDetails component layout and enhance favorite button functionality; improve description display and author listing

--- src/widgets/BookPage/BookDetails/BookDetails.tsx
+++ src/widgets/BookPage/BookDetails/BookDetails.tsx
@@ -28,33 +28,43 @@ const BookDetails = ({ book }: { book: Book }) => {
   return (
     <div className="flex flex-col md:flex-row gap-6 bg-white p-6 rounded-lg shadow-lg w-5/6 ">
       {/* Book Cover + Favorite Button */}
-      <div className="relative">
+      <div className="w-40 h-70 flex flex-col items-center">
+        <div className="w-full flex justify-end">
+          <button
+            className={`text-3xl transition-all duration-300 ease-in-out ${isFavorite ? "text-red-500 hover:scale-110 hover:drop-shadow-md" : "text-gray-300 hover:text-red-400 hover:scale-110 hover:drop-shadow-md"}`}
+            onClick={() => setIsFavorite(!isFavorite)}
+          >
+            <FaHeart />
+          </button>
+        </div>
         <img
           src={`https://covers.openlibrary.org/b/id/${book.covers?.[0]}-L.jpg`}
-          className="w-40 max-w-md p-3 h-auto rounded-md shadow-md mt-6"
+          className="w-40 max-w-md p-3 h-auto rounded-md shadow-md mt-2"
           alt={book.title}
         />
-        <button
-          className={`absolute top-2 -right-200 text-3xl ${
-            isFavorite ? "text-red-500" : "text-gray-300"
-          }`}
-          onClick={() => setIsFavorite(!isFavorite)}
-        >
-          <FaHeart />
-        </button>
       </div>
+
       {/* Book Information */}
       <div className="flex flex-col justify-center">
-        <h1 className="text-2xl font-ravi">{book.title}</h1>
-        <p className="text-gray-700 italic break-words w-auto pr-1.5">
+        <h1 className="text-2xl font-ravi pl-0.5">{book.title}</h1>
+        <p className="overflow-hidden max-h-[100px] hover:max-h-[500px] transition-all duration-300 ease-in-out">
           {typeof book.description === "string"
             ? book.description
             : book.description?.value || "No description found..."}
+          {typeof book.description === "string"
+            ? book.description.length > 100
+              ? `${book.description.substring(0, 100)}...`
+              : book.description
+            : book.description?.value.length > 100
+              ? `${book.description.value.substring(0, 100)}...`
+              : book.description?.value || "No description found..."}
         </p>
         <p className="font-ravi text-xl text-gray-900 mt-2">
+          Author:
+          {<br></br>}
           {authors.length > 0
             ? authors.map((author, index) => (
-                <span key={author.key || index}>{author.name}</span>
+                <span key={author.key || index}> {author.name}</span>
               ))
             : "Unknown"}
         </p>
