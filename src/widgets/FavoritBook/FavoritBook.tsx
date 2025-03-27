6ce9106626bf704bd24611aec01d276721de60ec
Sabina Mejlisson <sabinamejlisson@gmail.com>
2025-03-27 11:32:53 +0100

    Add functionality to remove FavoritBooks

--- src/widgets/FavoritBook/FavoritBook.tsx
+++ src/widgets/FavoritBook/FavoritBook.tsx
@@ -3,7 +3,7 @@ import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const FavoritBooks = () => {
-    const { favoritBooks } = useFavoritBooks();
+    const { favoritBooks, toggleFavorite } = useFavoritBooks();
 
     const generateSlug = (title: string) =>
         title.toLowerCase().replace(/\s+/g, "-").replace(/[^a-z0-9\-]/g, "");
@@ -15,19 +15,31 @@ const FavoritBooks = () => {
                 {favoritBooks.map((book) => {
                     const slug = generateSlug(book.title);
                     return (
-                        <Link
-                            to={`/book/${book.key.replace("/works/", "")}/${slug}`}
+                        <div
                             key={book.key}
                             className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center hover:shadow-lg hover:scale-105 transition-transform"
                         >
-                            <img
-                                src={`https://covers.openlibrary.org/b/id/${book.covers?.[0]}-L.jpg`}
-                                alt={book.title}
-                                className="w-32 h-auto rounded-md"
-                            />
-                            <FaHeart className="text-red-500 mt-2" />
-                            <p className="text-center font-semibold mt-2">{book.title}</p>
-                        </Link>
+                            <Link
+                                to={`/book/${book.key.replace("/works/", "")}/${slug}`}
+                                className="flex flex-col items-center"
+                            >
+                                <img
+                                    src={`https://covers.openlibrary.org/b/id/${book.covers?.[0]}-L.jpg`}
+                                    alt={book.title}
+                                    className="w-32 h-auto rounded-md"
+                                />
+                                <p className="text-center font-semibold mt-2">{book.title}</p>
+                            </Link>
+
+
+                            <button
+                                className="mt-2 text-red-500 hover:text-red-400 hover:scale-200 duration-400 text-xl"
+                                onClick={() => toggleFavorite(book)}
+                                title="Remove from favorites"
+                            >
+                                <FaHeart />
+                            </button>
+                        </div>
                     );
                 })}
             </div>
